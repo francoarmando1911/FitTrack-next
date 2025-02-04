0e33f187e2123a070af925016d3f750ecb77f13e
Franco Armando <francoarmando1911@gmail.com>
2025-02-04 19:07:34 -0300

    Componente de calorias

--- src/components/ResumeCaloriesComponent.tsx
+++ src/components/ResumeCaloriesComponent.tsx
@@ -4,24 +4,27 @@ import React from "react";
 
 export default function ResumeCaloriesComponent() {
     return (
-        <div className="bg-gradient-to-br from-orange-500 to-rose-600 min-h-screen flex flex-col items-center justify-center py-10">
-            <h2 className="text-4xl font-black text-white text-center mb-10">
-                Resumen de Calorías
-            </h2>
+        <section className="bg-gray-800 min-h-screen flex items-center justify-center px-4 py-10">
+            <div className="container mx-auto max-w-lg bg-white/95 backdrop-blur-md rounded-2xl shadow-xl p-8">
+                <h2 className="text-4xl font-black text-rose-600 text-center mb-10 pt-10">
+                    Resumen de Calorías
+                </h2>
 
-            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 w-full max-w-4xl">
-                <div className="bg-white text-center p-6 rounded-lg shadow-lg w-full md:w-auto">
-                    <p className="text-xl font-bold text-gray-800 mb-3">
-                        <span className="font-black text-6xl text-orange-500">250 </span>
-                        Consumidas en alimentos
-                    </p>
+                <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 w-full max-w-4xl">
+                    <div className="bg-white text-center p-6 rounded-lg shadow-lg w-full md:w-auto">
+                        <p className="text-xl font-bold text-gray-800 mb-3">
+                            <span className="font-black text-6xl text-orange-500">250 </span>
+                            Consumidas en alimentos
+                        </p>
 
-                    <p className="text-xl font-bold text-gray-800 mb-3">
-                        <span className="font-black text-6xl text-orange-500">250 </span>
-                        Quemadas en ejercicio
-                    </p>
+                        <p className="text-xl font-bold text-gray-800 mb-3">
+                            <span className="font-black text-6xl text-orange-500">250 </span>
+                            Quemadas en ejercicio
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+        
     );
 }

7740300099a554a0ea56ef703c539c10d81af94c
Franco Armando <francoarmando1911@gmail.com>
2025-02-04 23:34:54 -0300

    actualizacion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "../styles/globals.css";
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );

c60255d0c74bacb82fec7a620e8808b6f1d75fd9
Franco Armando <francoarmando1911@gmail.com>
2025-02-06 10:56:59 -0300

    actualizacion nuevo componente

--- src/app/paginaCalorias/page.tsx
+++ src/app/paginaCalorias/page.tsx
@@ -5,6 +5,7 @@ import CaloriesComponent from "@/components/CaloriesComponent";
 import { activityReducer, initialState } from "@/reducers/activity-reducers";
 import ResumeCaloriesComponent from "@/components/ResumeCaloriesComponent";
 import FooterComponent from "@/components/FooterComponent";
+import ActivityList from "@/components/ActivityList";
 
 export default function Page() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -13,6 +14,7 @@ export default function Page() {
     <div>
       <CaloriesComponent dispatch={dispatch} state={state} />
       <ResumeCaloriesComponent activities={state.activities || []} />
+      <ActivityList/>
       <FooterComponent/>
     </div>
   );
---
+++ src/components/ActivityList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ActivityList() {
+  return (
+    <div className='items-center justify-center'>
+        <h1 className='text-6xl text-red-800 pt-10 font-bold'>Comida y Actividades</h1>
+
+        <span className='pt-5'>prueuba</span>
+    </div>
+  )
+}
